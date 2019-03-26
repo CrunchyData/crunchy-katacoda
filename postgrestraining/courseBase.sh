16c0eb8207e170d1408f3059e67e63e102f9948f
Dave Cramer <davecramer@gmail.com>
2019-03-25 21:03:39 -0400

    echo progress

--- postgrestraining/courseBase.sh
+++ postgrestraining/courseBase.sh
@@ -3,13 +3,16 @@
 
 set -e -u
 
+echo 'adding postgres rpm'
 yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-centos10-10-2.noarch.rpm
 
+echo 'installing postgres and backrest'
 yum install -y postgresql10 pgbackrest
 
 
 docker network create --driver bridge pgnet
 
+echo 'starting postgres docker container'
 docker run \
     --publish 5432:5432 \
     --volume pgdata:/pgdata \
@@ -26,6 +29,7 @@ docker run \
     --hostname=primary \
     --detach crunchydata/crunchy-postgres:centos7-11.2-2.3.1
 
+echo 'starting pgadmin container'
 docker run \
     --publish 5050:5050 \
     --volume pgadmin:/var/lib/pgadmin \
@@ -35,3 +39,5 @@ docker run \
     --name=pgadmin \
     --hostname=pgadmin \
     --detach crunchydata/crunchy-pgadmin4:centos7-11.2-2.3.1
+echo 'done'
+
