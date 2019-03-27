f2ace9a11067d6d08479a04619f89c1c3f68b5b7
Ben Hall <Ben@Benhall.me.uk>
2019-03-27 18:25:56 +0000

    Update environments based on scenario

--- environments/crunchydata-base1/build/1_install-packages.sh
+++ environments/crunchydata-base1/build/1_install-packages.sh
@@ -1,8 +1,15 @@
 echo "Installing required packages"
 
-yum install -y postgresql-server postgresql-contrib
+echo 'adding postgres rpm'
+yum install -y yum install https://download.postgresql.org/pub/repos/yum/11/redhat/rhel-7-x86_64/pgdg-centos11-11-2.noarch.rpm
+
+echo 'installing postgres and backrest'
+yum install -y postgresql11 pgbackrest postgresql-contrib
 
 postgresql-setup initdb
 systemctl start postgresql
 systemctl enable postgresql
 
+echo "Pull Docker Images"
+docker pull crunchydata/crunchy-postgres:centos7-11.2-2.3.1
+docker pull crunchydata/crunchy-pgadmin4:centos7-11.2-2.3.1
