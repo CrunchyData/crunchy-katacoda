6560b521f2d1e93d52eab6f5d1b5efa0c8691609
Steven Pousty <steve.pousty@gmail.com>
2019-03-28 15:11:40 -0700

    updated for the base image

--- environments/crunchydata-single1/build/1_install-packages.sh
+++ environments/crunchydata-single1/build/1_install-packages.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 echo "Installing required packages"
 
 echo 'adding postgres rpm'
 yum install -y yum install https://download.postgresql.org/pub/repos/yum/11/redhat/rhel-7-x86_64/pgdg-centos11-11-2.noarch.rpm
 
 echo 'installing postgres and backrest'
-yum install -y postgresql11 pgbackrest postgresql-contrib
+yum install -y postgresql11 pgbackrest postgresql-contrib postgis25_11
 postgresql-setup initdb
 
 echo "Pull Docker Images"
@@ -12,5 +14,12 @@ docker pull crunchydata/crunchy-postgres:centos7-11.2-2.3.1
 docker pull crunchydata/crunchy-postgres-gis:centos7-11.2-2.3.1
 docker pull crunchydata/crunchy-pgadmin4:centos7-11.2-2.3.1
 docker pull crunchydata/crunchy-pgpool:centos7-11.2-2.3.1
-
 docker pull thesteve0/postgres-appdev
+
+echo "Downloading sample data"
+cd /tmp
+wget https://github.com/thesteve0/crunchydemodata/releases/download/v0.1/crunchy_demo_data_v0.1.zip
+mkdir /data
+chmod 777 /data
+unzip crunchy_demo_data_v0.1.zip -d /data
+rm -f crunchy_demo_data_v0.1.zip
