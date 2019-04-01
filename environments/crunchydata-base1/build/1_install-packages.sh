56a701d513728ac3d82c2e824df85c011c762354
Steven Pousty <scitronpousty@gmail.com>
2019-04-01 15:30:10 -0700

    getting up to date (#1)
    * Update environments based on scenario
    
    * updated for the base image
    
    * updated for the base image
    
    * updated for the base image
    
    * install the server as well\
    use the correct path for initdb\
    start and enable the server
    
    * get the correct package name for the server
    
    * fix step1 to start version 11

--- environments/crunchydata-base1/build/1_install-packages.sh
+++
@@ -1,8 +0,0 @@
-echo "Installing required packages"
-
-yum install -y postgresql-server postgresql-contrib
-
-postgresql-setup initdb
-systemctl start postgresql
-systemctl enable postgresql
-
---
+++ environments/crunchydata-single1/build/1_install-packages.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Installing required packages"
+
+echo 'adding postgres rpm'
+yum install -y yum install https://download.postgresql.org/pub/repos/yum/11/redhat/rhel-7-x86_64/pgdg-centos11-11-2.noarch.rpm
+
+echo 'installing postgres and backrest'
+yum install -y postgresql11 postgresql11-server  pgbackrest postgresql-contrib postgis25_11 pgadmin4 unzip
+/usr/pgsql-11/bin/postgresql-11-setup initdb
+
+systemctl start postgresql11
+systemctl enable postgresql11
+
+
+echo "Pull Docker Images"
+docker pull crunchydata/crunchy-postgres:centos7-11.2-2.3.1
+docker pull crunchydata/crunchy-postgres-gis:centos7-11.2-2.3.1
+docker pull crunchydata/crunchy-pgadmin4:centos7-11.2-2.3.1
+docker pull crunchydata/crunchy-pgpool:centos7-11.2-2.3.1
+docker pull thesteve0/postgres-appdev
+
+echo "Downloading sample data"
+cd /tmp
+wget https://github.com/thesteve0/crunchydemodata/releases/download/v0.1/crunchy_demo_data_v0.1.zip
+mkdir /data
+chmod 777 /data
+unzip crunchy_demo_data_v0.1.zip -d /data
+rm -f crunchy_demo_data_v0.1.zip
