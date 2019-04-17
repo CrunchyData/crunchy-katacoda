1b8fc60d14ebad154783f1276dbc01d3397eb15e
Steven Pousty <scitronpousty@gmail.com>
2019-04-16 17:19:58 -0700

    Fixed data download and RPM repo
    @BenHall

--- environments/crunchydata-single1/build/1_install-packages.sh
+++ environments/crunchydata-single1/build/1_install-packages.sh
@@ -39,7 +39,7 @@ docker pull thesteve0/postgres-appdev
 
 echo "Downloading sample data"
 cd /tmp
-wget https://github.com/thesteve0/crunchydemodata/releases/download/v0.2/crunchy_demo_data_v0.2.zip
+wget https://github.com/CrunchyData/crunchy-demo-data/releases/download/v0.2/crunchy_demo_data_v0.2.zip
 mkdir /data
 chmod 777 /data
 unzip crunchy_demo_data_v0.2.zip -d /data
