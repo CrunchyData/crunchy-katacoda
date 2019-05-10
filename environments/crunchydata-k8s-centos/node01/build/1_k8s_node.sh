9e51ce52a35f52a1b73880c24112470ce7546d48
Ben Hall <ben@benhall.me.uk>
2019-05-10 11:45:42 +0100

    New 2node k8s env

---
+++ environments/crunchydata-k8s-centos/node01/build/1_k8s_node.sh
@@ -0,0 +1,31 @@
+echo '127.0.0.1 node01' >> /etc/hosts
+hostname node01 && echo node01 > /etc/hostname
+hostnamectl set-hostname node01
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+systemctl enable --now kubelet
+
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+sudo kubeadm config images pull
+sudo docker pull weaveworks/weave-kube:2.5.1
+sudo docker pull weaveworks/weave-npc:2.5.1
