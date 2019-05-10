9384e2315a020ad531cca705e7680f4ec5422c09
Ben Hall <ben@benhall.me.uk>
2019-05-10 17:27:38 +0100

    Remove Docker first

--- environments/crunchydata-k8s-centos/node01/build/1_k8s_node.sh
+++ environments/crunchydata-k8s-centos/node01/build/1_k8s_node.sh
@@ -1,3 +1,5 @@
+sudo yum remove -y docker
+
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce docker-ce-cli containerd.io
