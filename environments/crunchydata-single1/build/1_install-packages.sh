b4d6d46a0f0305742fbcfbe0798e832097928d99
Steven Pousty <steve.pousty@gmail.com>
2019-05-24 10:06:05 -0700

    steve learns not to put in binaries if at all possible

--- environments/crunchydata-single1/build/1_install-packages.sh
+++ environments/crunchydata-single1/build/1_install-packages.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ #!/bin/bash
 
 echo "Installing required packages"
 
--- postgis/qpostgisintro/set-env.sh
+++ postgis/qpostgisintro/set-env.sh
@@ -6,16 +6,11 @@ echo 'please wait while we prep the environment (should take about 10 seconds)'
 echo 'starting the database'
 docker run -d -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=workshop --name=pgsql thesteve0/postgres-appdev && sleep 8
 
-echo 'loading county boundaries'
-PGPASSWORD="password" psql -h localhost -U groot -f /data/crunchy_demo_data/boundaries/county_boundaries.ddl.sql workshop
-PGPASSWORD="password" psql -h localhost -U groot -d workshop -c '\COPY county_geometry (statefp, countyfp, countyns, geoid, county_name, namelsad, funcstat, aland, awater, interior_pnt, the_geom) from '\''/data/crunchy_demo_data/boundaries/county_boundaries_copy.txt'\'' WITH CSV QUOTE '\''"'\'' '
-echo 'finished county boundaries'
+unzip
 
 echo 'loading storm center points'
 PGPASSWORD="password" psql -h localhost -U groot -f /data/crunchy_demo_data/storms/stormevents.ddl.sql workshop
 PGPASSWORD="password" psql -h localhost -U groot -d workshop -c '\COPY se_locations (episode_id, event_id, location_index, range, azimuth, location, latitude, longitude, the_geom) from '\''/data/crunchy_demo_data/storms/storm_locations_copy.txt'\'' WITH CSV QUOTE '\''"'\'' '
 echo 'finished storm center points'
-
-clear
 
 echo 'ready to go!'
