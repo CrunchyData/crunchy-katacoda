5f9917fa1f2f43c0a0a52954a29ab2526daf2c63
Ben Hall <ben@benhall.me.uk>
2019-05-25 10:01:50 +0200

    Remove sleeps and poll for data instead

--- basic-postgresql-devel/qftextintro/set-env.sh
+++ basic-postgresql-devel/qftextintro/set-env.sh
@@ -2,7 +2,12 @@
 
 echo 'please wait while we prep the environment (should take about 10 seconds)'
 echo 'starting the database'
-docker run -d -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=workshop --name=pgsql thesteve0/postgres-appdev && sleep 8
+docker run -d -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=workshop --name=pgsql thesteve0/postgres-appdev
+
+until PGPASSWORD="password" psql -h localhost -U groot postgres -c '\l' &> /dev/null; do
+  echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Postgres to start"
+  sleep 1
+done
 
 echo 'loading storm center points'
 PGPASSWORD="password" psql -h localhost -U groot -f /data/crunchy_demo_data/storms/stormevents.ddl.sql workshop
--- postgis/spatial_relationships/set-env.sh
+++ postgis/spatial_relationships/set-env.sh
@@ -4,7 +4,12 @@
 
 echo 'please wait while we prep the environment (should take about 10 seconds)'
 echo 'starting the database'
-docker run -d -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=nyc --name=pgsql thesteve0/postgres-appdev && sleep 8
+docker run -d -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=nyc --name=pgsql thesteve0/postgres-appdev
+
+until PGPASSWORD="password" psql -h localhost -U groot postgres -c '\l' &> /dev/null; do
+  echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Postgres to start"
+  sleep 1
+done
 
 echo 'loading data'
 curl http://s3.cleverelephant.ca/nyc_data.sql.gz |gzip -dc | PGPASSWORD="password" psql -h localhost -U groot nyc
