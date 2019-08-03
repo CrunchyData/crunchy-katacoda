f7e6d967fefdf6047bf335ace633c4e77db887a6
Steven Pousty <steve.pousty@gmail.com>
2019-08-03 15:01:42 -0700

    new data release

--- environments/crunchydata-single1/build/1_install-packages.sh
+++ environments/crunchydata-single1/build/1_install-packages.sh
@@ -31,15 +31,15 @@ fi
 /usr/pgsql-11/bin/postgresql-11-setup initdb
 
 echo "Pull Docker Images"
-docker pull crunchydata/crunchy-postgres:centos7-11.2-2.3.1
-docker pull crunchydata/crunchy-postgres-gis:centos7-11.2-2.3.1
-docker pull crunchydata/crunchy-pgadmin4:centos7-11.2-2.3.1
-docker pull crunchydata/crunchy-pgpool:centos7-11.2-2.3.1
+docker pull crunchydata/crunchy-postgres:centos7-11.4-2.4.1
+docker pull crunchydata/crunchy-postgres-gis:centos7-11.4-2.4.1
+docker pull crunchydata/crunchy-pgadmin4:centos7-11.4-2.4.1
+docker pull crunchydata/crunchy-pgpool:centos7-11.4-2.4.1 
 docker pull thesteve0/postgres-appdev
 
 echo "Downloading sample data"
 cd /tmp
-wget https://github.com/CrunchyData/crunchy-demo-data/releases/download/v0.2/crunchy_demo_data_v0.2.zip
+wget https://github.com/CrunchyData/crunchy-demo-data/releases/download/v0.3/crunchy_demo_data_v0.3.zip
 mkdir /data
 chmod 777 /data
 unzip crunchy_demo_data_v0.2.zip -d /data
