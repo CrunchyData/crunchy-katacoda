63a880a9989d9cc062f0df5430a719a6b608e868
Keith Fiske <keith@keithf4.com>
2019-08-27 18:12:55 -0400

    Give training sudo

--- pg-administration/basic-postgresql-for-dbas/setting-up-pg/env-init.sh
+++ pg-administration/basic-postgresql-for-dbas/setting-up-pg/env-init.sh
@@ -5,6 +5,7 @@
 adduser training
 echo "training" | passwd training --stdin
 usermod -aG wheel training
+echo "training ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 yum -y erase postgresql10-libs postgresql11*
 yum -y remove pgdg-redhat-repo
