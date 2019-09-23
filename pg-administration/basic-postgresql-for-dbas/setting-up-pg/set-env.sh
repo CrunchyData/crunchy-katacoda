72b8fc8d409dea85a53eb9176ffe3dea698e06f2
Keith Fiske <keith@keithf4.com>
2019-09-23 16:21:57 -0400

    Fix changing to training user for pgadmin scenarios

--- pg-administration/basic-postgresql-for-dbas/setting-up-pg/set-env.sh
+++ pg-administration/basic-postgresql-for-dbas/setting-up-pg/set-env.sh
@@ -2,13 +2,12 @@
 
 #runs in foreground
 
-CURRENT_USER = $(whoami)
+CURRENT_USER=$(whoami)
 
 until [ $CURRENT_USER == "training" ]; do
-    echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for current user to change to training..."
-    sleep 1;
     sudo -iu training
     CURRENT_USER = $(whoami)
-
+    echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for current user to change to training..."
+    sleep 1;
 done
 
