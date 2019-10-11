1fb663f00e8a600a53d3295bf55026d5da8794e9
steve pousty <steve.pousty@gmail.com>
2019-10-11 11:00:16 -0700

    .\basic-postgresql-devel\qjsonintro\env-init.sh

--- basic-postgresql-devel/qjsonintro/env-init.sh
+++ basic-postgresql-devel/qjsonintro/env-init.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
+docker network create mybridge
 
 echo 'starting the database'
-docker run -d -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=workshop --name=pgsql thesteve0/postgres-appdev
+docker run -d -network mybridge -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=workshop --name=pgsql thesteve0/postgres-appdev
 
 until PGPASSWORD="password" psql -h localhost -U groot postgres -c '\l' &> /dev/null; do
   echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Postgres to start"
