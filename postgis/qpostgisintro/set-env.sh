855b74fcbdba7d58b0f71110030cf09a07df767f
steve pousty <steve.pousty@gmail.com>
2019-10-11 19:46:32 -0700

    getting everyone on docker network

--- postgis/qpostgisintro/set-env.sh
+++ postgis/qpostgisintro/set-env.sh
@@ -4,7 +4,8 @@
 
 echo 'please wait while we prep the environment (should take about 10 seconds)'
 echo 'starting the database'
-docker run -d -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=workshop --name=pgsql thesteve0/postgres-appdev 
+docker network create mybridge
+docker run -d --network mybridge -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=workshop --name=pgsql thesteve0/postgres-appdev
 
 until PGPASSWORD="password" psql -h localhost -U groot postgres -c '\l' &> /dev/null; do
   echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Postgres to start"
