22f61d0294b2c0feef213086702a6f4cb3c89d0c
Keith Fiske <keith@keithf4.com>
2020-03-11 12:50:14 -0400

    Fix setting-up-pg env-init

--- pg-administration/basic-postgresql-for-dbas-11/setting-up-pg/env-init.sh
+++ pg-administration/basic-postgresql-for-dbas-11/setting-up-pg/env-init.sh
@@ -10,4 +10,4 @@ echo "training ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 yum -y erase postgresql10-libs postgresql11* postgresql12*
 yum -y remove pgdg-redhat-repo
 
-rm -rf /var/lib/pgsql
+rm -rf /var/lib/pgsql/*
