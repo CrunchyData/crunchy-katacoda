d70549503ecc16b09205d57035ad9466fb863be6
kbatuigas <36839689+kbatuigas@users.noreply.github.com>
2020-04-22 17:53:23 -0400

    Move cat statements higher

--- postgresql-devel/basics/basicdatatype/set-env.sh
+++ postgresql-devel/basics/basicdatatype/set-env.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/bash
 
 echo 'please wait while we prep the environment (should take about 10 seconds)'
-echo 'starting the database'
-docker network create mybridge
-
-docker run -d --network mybridge -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=workshop --name=pgsql crunchydata/crunchy-postgres-appdev:latest
-
-until PGPASSWORD="password" psql -h localhost -U groot postgres -c '\l' &> /dev/null; do
-  echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Postgres to start"
-  sleep 1
-done
 
 cat <<EOF > employees-ddl.sql
 -- Employee table
@@ -132,6 +123,16 @@ UNION
 SELECT 4, '2016-03-01'::date , 40000.00, null
 ;
 EOF
+
+echo 'starting the database'
+docker network create mybridge
+
+docker run -d --network mybridge -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=workshop --name=pgsql crunchydata/crunchy-postgres-appdev:latest
+
+until PGPASSWORD="password" psql -h localhost -U groot postgres -c '\l' &> /dev/null; do
+  echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Postgres to start"
+  sleep 1
+done
 
 until PGPASSWORD="password" psql -h localhost -U groot -f employees-ddl.sql workshop &> /dev/null; do
   echo >&2 "$(date +%Y%m%dt%H%M%S) loading employee schema"
