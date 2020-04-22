f6c5503e622fa4882916235cf7cc10101e587604
thesteve0 <steve.pousty@gmail.com>
2020-04-22 15:33:40 -0700

    trying to protect the user

--- kube/basics/introop/set-env.sh
+++ kube/basics/introop/set-env.sh
@@ -10,4 +10,14 @@
 #now source the env variables for the client
 #source ~/.bashrc
 
-launch.sh; kubectl wait pod -n pgo -l name=postgres-operator --for=condition=ready
+#until PGPASSWORD="password" psql -h localhost -U groot postgres -c '\l' &> /dev/null; do
+#  echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Postgres to start"
+#  sleep 1
+#done
+
+launch.sh; # kubectl wait pod -n pgo -l name=postgres-operator --for=condition=ready
+
+until kubectl get pods -n pgo &> /dev/null; do
+  echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting The Operator to Start"
+  sleep 1
+done
