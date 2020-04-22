19fd7ec48018a5f571bf25533ecb0882972ed0c9
kbatuigas <36839689+kbatuigas@users.noreply.github.com>
2020-04-21 21:22:48 -0400

    Troubleshooting set-env

--- postgresql-devel/basics/basicdatatype/set-env.sh
+++ postgresql-devel/basics/basicdatatype/set-env.sh
@@ -125,17 +125,17 @@ VALUES (
 -- Add salary history
 INSERT INTO employee_salary_hist (
     employee_id, employee_salary_start_date, employee_salary_amount, employee_salary_end_date)
-SELECT 1, '2016-03-01'::date , 40000.00, '2017-02-28'::date
+SELECT 1, '2016-03-01'::date, 40000.00, '2017-02-28'::date
 UNION
-SELECT 1, '2017-03-01'::date , 50000.00, null
+SELECT 1, '2017-03-01'::date, 50000.00, NULL
 UNION
-SELECT 2, '2016-03-01'::date , 40000.00, '2017-02-28'::date
+SELECT 2, '2016-03-01'::date, 40000.00, '2017-02-28'::date
 UNION
-SELECT 2, '2016-04-01'::date , 40000.00, null
+SELECT 2, '2016-04-01'::date, 40000.00, NULL
 UNION
-SELECT 3, '2016-03-01'::date , 40000.00, null
+SELECT 3, '2016-03-01'::date, 40000.00, NULL
 UNION
-SELECT 4, '2016-03-01'::date , 40000.00, null
+SELECT 4, '2016-03-01'::date, 40000.00, NULL
 ;
 
 EOF
