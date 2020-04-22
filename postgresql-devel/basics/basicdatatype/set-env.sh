5ae3c5244a206931e130a8bd71219ab4b5e503b0
kbatuigas <36839689+kbatuigas@users.noreply.github.com>
2020-04-22 16:23:44 -0400

    Set-env appears to not run if scenario is started very quickly
    Seems to load fine if I wait for it to load in the background
    (i.e. read the entire intro)
    
    Try to see if wrapping psql statement in wait loop helps

--- postgresql-devel/basics/basicdatatype/set-env.sh
+++ postgresql-devel/basics/basicdatatype/set-env.sh
@@ -6,7 +6,7 @@ docker network create mybridge
 
 docker run -d --network mybridge -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=workshop --name=pgsql crunchydata/crunchy-postgres-appdev:latest
 
-until PGPASSWORD="password" psql -h localhost -U groot postgres -c '\l' &> /dev/null; do
+until PGPASSWORD="password" psql -h localhost -U groot -f employees-ddl.sql workshop &> /dev/null; do
   echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Postgres to start"
   sleep 1
 done
