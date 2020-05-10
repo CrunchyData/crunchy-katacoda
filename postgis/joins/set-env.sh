518692f40dbf53db97f99a613045c4dbb135f025
thesteve0 <steve.pousty@gmail.com>
2020-05-10 12:27:26 -0700

    more moving to newer containers

--- postgis/joins/set-env.sh
+++ postgis/joins/set-env.sh
@@ -3,7 +3,7 @@
 echo 'Please wait while we prep the environment (should take about 10 seconds)'
 echo 'Starting the database...'
 
-docker run -d -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=nyc --name=pgsql thesteve0/postgres-appdev
+docker run -d -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=nyc --name=pgsql crunchydata/crunchy-postgres-appdev
 
 until PGPASSWORD="password" psql -h localhost -U groot postgres -c '\l' &> /dev/null; do
   echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Postgres to start"
