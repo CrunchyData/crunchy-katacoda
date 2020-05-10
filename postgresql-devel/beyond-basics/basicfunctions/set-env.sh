ceac11e20dd5e1930f84606830f1eb358ef7c778
thesteve0 <steve.pousty@gmail.com>
2020-05-10 13:28:29 -0700

    4.18 of admin works in FF, got rid of old images, and removed unused categories

--- postgresql-devel/beyond-basics/basicfunctions/set-env.sh
+++ postgresql-devel/beyond-basics/basicfunctions/set-env.sh
@@ -4,7 +4,7 @@ echo 'please wait while we prep the environment (should take about 10 seconds)'
 echo 'starting the database'
 docker network create mybridge
 
-docker run -d --network mybridge -p 5050:5050 -e PGADMIN_SETUP_EMAIL=admin -e PGADMIN_SETUP_PASSWORD=password --name=pgadmin crunchydata/crunchy-pgadmin4:centos7-12.2-4.2.2
+docker run -d --network mybridge -p 5050:5050 -e PGADMIN_SETUP_EMAIL=admin -e PGADMIN_SETUP_PASSWORD=password --name=pgadmin crunchydata/crunchy-pgadmin4:centos7-12.2-4.3.0
 
 docker run -d --network mybridge -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=workshop --name=pgsql crunchydata/crunchy-postgres-appdev
 
