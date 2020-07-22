a717f391241e54d75ded80811b73f904f84c48b5
kbatuigas <36839689+kbatuigas@users.noreply.github.com>
2020-07-22 11:58:40 -0400

    Add dir for Intro to PL/Python scenario

---
+++ postgresql-devel/beyond-basics/plpy/env-init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+echo 'starting the database'
+docker network create mybridge
+docker run -d --network mybridge -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=workshop --name=pgsql crunchydata/crunchy-postgres-appdev
+
+until PGPASSWORD="password" psql -h localhost -U groot postgres -c '\l' &> /dev/null; do
+  echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Postgres to start"
+  sleep 1
+done
+
+PGPASSWORD="password" psql -h localhost -U groot workshop
