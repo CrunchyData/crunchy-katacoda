025d930af6196fb693ed20e78efb644e1ea078bf
kbatuigas <36839689+kbatuigas@users.noreply.github.com>
2020-07-27 12:25:23 -0400

    Test the environment

--- postgresql-devel/beyond-basics/plpy/set-env.sh
+++ postgresql-devel/beyond-basics/plpy/set-env.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/bash
 
-# Nothing Yet
+echo 'please wait while we prep the environment (should take about 10 seconds)'
+echo 'starting the database'
+docker network create mybridge
+
+docker run -d --network mybridge -p 5050:5050 -e PGADMIN_SETUP_EMAIL=admin -e PGADMIN_SETUP_PASSWORD=password --name=pgadmin crunchydata/crunchy-pgadmin4:centos7-12.2-4.3.0
+
+docker run -d --network mybridge -p 5432:5432 -e PG_USER=groot -e PG_PASSWORD=password -e PG_DATABASE=workshop --name=pgsql crunchydata/crunchy-postgres-appdev
+
+until PGPASSWORD="password" psql -h localhost -U groot postgres -c '\l' &> /dev/null; do
+  echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Postgres to start"
+  sleep 1
+done
+
+PGPASSWORD="password" psql -h localhost -U groot workshop
