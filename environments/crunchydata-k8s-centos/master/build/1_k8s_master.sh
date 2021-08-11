f336c95870576ecff9bfc0c4675bed93765261be
Jonathan S. Katz <jonathan.katz@excoventures.com>
2021-08-11 12:59:04 -0400

    Update versions and source repo
    Switch source repo to install EL7 packages. Update to newer PGO
    version.

--- environments/crunchydata-k8s-centos/master/build/1_k8s_master.sh
+++ environments/crunchydata-k8s-centos/master/build/1_k8s_master.sh
@@ -5,8 +5,7 @@ set -e
 yum clean all
 yum makecache
 
-sudo rpm --import https://api.developers.crunchydata.com/downloads/gpg/RPM-GPG-KEY-crunchydata-dev
-sudo yum install -y https://api.developers.crunchydata.com/downloads/repo/rpm-centos/postgresql12/crunchypg12.repo
+sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 if [ "$?" -ne 0 ]; then
     echo "Unable to install Postgres Repo"
     exit 1
@@ -21,23 +20,23 @@ sudo yum install -y ansible
 cd /home/cent
 git clone https://github.com/CrunchyData/postgres-operator.git
 cd postgres-operator
-git checkout v4.7.0
+git checkout v4.7.1
 cd ansible
 
 echo "Pull recent docker images"
-sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-pgadmin4:centos8-12.7-4.7.0
-sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-pgbadger:centos8-12.7-4.7.0
-sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-pgbouncer:centos8-12.7-4.7.0
-sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-pgbackrest:centos8-12.7-4.7.0
-sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-pgbackrest-repo:centos8-12.7-4.7.0
-sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-postgres-ha:centos8-12.7-4.7.0
-sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-postgres-gis-ha:centos8-12.7-3.0-4.7.0
+sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-pgadmin4:centos8-12.7-4.7.1
+sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-pgbadger:centos8-12.7-4.7.1
+sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-pgbouncer:centos8-12.7-4.7.1
+sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-pgbackrest:centos8-12.7-4.7.1
+sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-pgbackrest-repo:centos8-12.7-4.7.1
+sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-postgres-ha:centos8-12.7-4.7.1
+sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-postgres-gis-ha:centos8-12.7-3.0-4.7.1
 
-sudo docker pull registry.developers.crunchydata.com/crunchydata/postgres-operator:centos8-4.7.0
-sudo docker pull registry.developers.crunchydata.com/crunchydata/pgo-apiserver:centos8-4.7.0
-sudo docker pull registry.developers.crunchydata.com/crunchydata/pgo-event:centos8-4.7.0
-sudo docker pull registry.developers.crunchydata.com/crunchydata/pgo-scheduler:centos8-4.7.0
-sudo docker pull registry.developers.crunchydata.com/crunchydata/pgo-client:centos8-4.7.0
-sudo docker pull registry.developers.crunchydata.com/crunchydata/pgo-rmdata:centos8-4.7.0
-sudo docker pull registry.developers.crunchydata.com/crunchydata/pgo-deployer:centos8-4.7.0
-sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-postgres-exporter:centos8-4.7.0
+sudo docker pull registry.developers.crunchydata.com/crunchydata/postgres-operator:centos8-4.7.1
+sudo docker pull registry.developers.crunchydata.com/crunchydata/pgo-apiserver:centos8-4.7.1
+sudo docker pull registry.developers.crunchydata.com/crunchydata/pgo-event:centos8-4.7.1
+sudo docker pull registry.developers.crunchydata.com/crunchydata/pgo-scheduler:centos8-4.7.1
+sudo docker pull registry.developers.crunchydata.com/crunchydata/pgo-client:centos8-4.7.1
+sudo docker pull registry.developers.crunchydata.com/crunchydata/pgo-rmdata:centos8-4.7.1
+sudo docker pull registry.developers.crunchydata.com/crunchydata/pgo-deployer:centos8-4.7.1
+sudo docker pull registry.developers.crunchydata.com/crunchydata/crunchy-postgres-exporter:centos8-4.7.1
